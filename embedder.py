f147ed159b61297d0f988632de3011d1e852a45a
sakharok13 <58632360+sakharok13@users.noreply.github.com>
2020-12-17 11:34:09 +0300

    Update embedder.py

--- embedder.py
+++ embedder.py
@@ -3,8 +3,8 @@ from torch import nn
 class Nnet(nn.Module):
     def __init__(self, embedding_dim=256, lstm_conf=None):
         super(Nnet, self).__init__()
-        self.encoder = nn.LSTM(input_size=128, hidden_size=1024) #input_size should be equal to n_mels 
-        self.linear = nn.Linear(1024, 256)
+        self.encoder = nn.LSTM(input_size=40, hidden_size=512) #input_size should be equal to n_mels 
+        self.linear = nn.Linear(512, 128)
 
     def forward(self, x):
         x = x.permute(0, 2, 1)
